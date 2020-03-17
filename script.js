d44c50a71081ae38b1049e7df641e6cbf470ba2e
Aakash-Kumar-Goryan <aakashgoryan@gmail.com>
2020-03-18 02:30:04 +0530

    Design & Script complete (Expecpected functionality done)

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ document.getElementById("my_form").addEventListener("submit", function (event) {
     console.log("Not Refreshed submission override");
     let myfile1 = document.getElementById('myfile1').files[0];
     let myfile2 = document.getElementById('myfile2').files[0];
-
-    let output = [];
-    // output.push('<li><strong>', escape(myfile1.name), '</strong> (', myfile1.type || 'n/a', ') - ',
-    //     myfile1.size, ' bytes, last modified: ',
-    //     myfile1.lastModifiedDate ? myfile1.lastModifiedDate.toLocaleDateString() : 'n/a',
-    //     '</li>');
-    // output.push('<li><strong>', escape(myfile2.name), '</strong> (', myfile2.type || 'n/a', ') - ',
-    //     myfile2.size, ' bytes, last modified: ',
-    //     myfile2.lastModifiedDate ? myfile2.lastModifiedDate.toLocaleDateString() : 'n/a',
-    //     '</li>');
-
     let read1 = new FileReader();
     let read2 = new FileReader();
     let res1;
@@ -23,17 +12,32 @@ document.getElementById("my_form").addEventListener("submit", function (event) {
     }
     read1.readAsBinaryString(myfile1);
     read2.onload = function () {
-        let res2 = read2.result
-        let set1 = new Set(res1.split("\n"));
-        let set2 = new Set(res2.split("\n"));
-        console.log(set1);
-        console.log(set2);
-        let difference = new Set([...set1].filter(x => !set2.has(x)));
-        console.log(difference);
-        for(let item of difference)
-        {
-            output.push(`<li>${item}</li>`);
-            document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+        let res2 = read2.result, 
+        set1 = new Set(res1.split("\n")), 
+        set2 = new Set(res2.split("\n")),
+        difference = new Set([...set1].filter(x => !set2.has(x))),
+        fileoutput = "",
+        output = `<table class="ui blue selectable table">
+                    <thead>
+                        <tr class="center aligned">
+                            <th>Missing items in 2nd file</th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+        for (let item of difference) {
+            output += `<tr class="center aligned"><td>${item}</td></tr>`;
+            fileoutput += item + '\n'
+        }
+        output += `</tbody><tfoot><tr class="center aligned"><th><i class="attention icon"></i>${difference.size} missing numbers</th></tr></tfoot></table>`
+        document.getElementById('container').innerHTML += `<a class="ui fluid blue button" id="myButton" href="#"><i class="download icon"></i> Save as text file</a>`;
+        document.getElementById('container').innerHTML += output;
+        document.getElementById('myButton').onclick = function (event) {
+            let blob = new Blob([fileoutput], { type: "text/plain;charset=utf-8" });
+            let blobUrl = window.URL.createObjectURL(blob);
+            this.href = blobUrl;
+            this.target = '_blank';
+            // target filename
+            this.download = 'file_compare_output.txt';
         }
     }
 })
